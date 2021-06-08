5d21e7dee17e3610943e2f8a9c90637487452b87
Freek-H <freek.hoogstede@gmail.com>
2021-06-08 20:05:26 +0200

    debugging .env configuration

--- nevo_api/views.py
+++ nevo_api/views.py
@@ -2,6 +2,16 @@ from django.http import HttpResponse
 import os
 # from dotenv import load_dotenv
 # load_dotenv()
+from . import settings
 
+x = os.environ.get('AZURE_PASSWORD')
+if x is not None:
+    x = x[0]
 def test(request):
-    return HttpResponse(f"The azure user is: {os.environ.get('AZURE_Uid')}@{os.environ.get('AZURE_HOST')}")
+    return HttpResponse(f"The azure user is: {os.environ.get('AZURE_USER')}@{os.environ.get('AZURE_HOST')}<br>\
+        {os.environ.get('AZURE_NAME')}<br>\
+        {os.environ.get('AZURE_USER')}<br>\
+        {x}<br>\
+        {os.environ.get('AZURE_HOST')}<br>\
+        {os.environ.get('AZURE_PORT')}<br>\
+        {settings.AZURE_USER}")
