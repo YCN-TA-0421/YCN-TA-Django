a28059a6ab1fc03de2f1e785ed8d8a3ee89bf030
Freek-H <freek.hoogstede@gmail.com>
2021-06-09 15:59:37 +0200

    added landing page with link to documentation

--- nevo_api/urls.py
+++ nevo_api/urls.py
@@ -20,5 +20,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('', views.test),
+    path('', views.index),
 ]
--- nevo_api/views.py
+++ nevo_api/views.py
@@ -1,17 +1,5 @@
 from django.http import HttpResponse
-import os
-# from dotenv import load_dotenv
-# load_dotenv()
-from . import settings
 
-x = os.environ.get('AZURE_PASSWORD')
-if x is not None:
-    x = x[0]
-def test(request):
-    return HttpResponse(f"The azure user is: {os.environ.get('AZURE_USER')}@{os.environ.get('AZURE_HOST')}<br>\
-        {os.environ.get('AZURE_NAME')}<br>\
-        {os.environ.get('AZURE_USER')}<br>\
-        {x}<br>\
-        {os.environ.get('AZURE_HOST')}<br>\
-        {os.environ.get('AZURE_PORT')}<br>\
-        {settings.AZURE_USER}")
+
+def index(request):
+    return HttpResponse(f"<a href='/api/docs/'>/api/docs/</a>")
