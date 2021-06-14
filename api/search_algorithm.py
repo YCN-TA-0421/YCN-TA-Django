9f37fe70562fb833abb1f4d72c8a468ea14e52ef
Freek-H <freek.hoogstede@gmail.com>
2021-06-14 22:29:24 +0200

    removed excess functions from search and added/updated docstrings

--- api/search_algorithm.py
+++ api/search_algorithm.py
@@ -1,16 +1,15 @@
 import numpy as np
-from datetime import date, datetime
-
-from numpy.core.numeric import indices
 from .models import Food
 from .variables import df
 
-WEIGHTS = (11, 8, 5, 3, 2, 1)
-EXACT_MATCH_MULT = 4
-NUM_WORDS_MULT = 1
+WEIGHTS = (11, 8, 5, 3, 2, 1)  # Weights for matching in the first, second, etc. word of the description
+EXACT_MATCH_MULT = 4  # Multiplier for an exact match
 
 
 def search_by_str(items, input_str):
+    """
+    Returns a list of scores with a score for every item in items based on how well that item matches with input_str.
+    """
     scores = np.zeros(len(items))
     for one_word in input_str.strip().split():
         for index_item, one_item in enumerate(items):
@@ -25,19 +24,18 @@ def search_by_str(items, input_str):
 
 
 def convert_scores_to_indices(scores):
+    """
+    Converts the scores to the matching indexes if the score is above zero and then sorts them by score, descending.
+    """
     return sorted([index for index, one_score in enumerate(scores) if one_score > 0], key=lambda index: scores[index], reverse=True)
 
 
-def convert_scores_to_result(df, scores):
-    return df.loc[sorted([index for index, one_score in enumerate(scores) if one_score > 0], key=lambda index: scores[index], reverse=True)], sorted([one_score for one_score in scores if one_score > 0], reverse=True)
-
-
-def search_by_str_from_df(df, column, input_str):
-    scores = search_by_str(df[column].values, input_str)
-    return convert_scores_to_result(df, scores)
-
-
 def recombine_words(df):
+    """
+    If the first two words in the Product_omschrijving are in the form of "ui zilver-", the dash is removed, the words swapped and concatenated.
+    Any other words are concatenated back onto the new word with spaces as separators.
+    Returns a list with an item for every item in the input list, either modified as needed or untouched.
+    """
     results = []
     for one_item in df['Product_omschrijving'].values:
         splits = one_item.strip().split()
@@ -50,6 +48,9 @@ def recombine_words(df):
 
 
 def search_indices(input_str):
+    """
+    Returns the indices of the best matching items ordered by score, descending.
+    """
     indices = convert_scores_to_indices(search_by_str(df['Product_omschrijving'].values, input_str))
 
     if not indices:
@@ -62,6 +63,9 @@ def search_indices(input_str):
 
 
 def search_objects(input_str):
+    """
+    Returns a list of at most 10 food object that best match the input_str by name, recombined name, or synonym.
+    """
     scores = search_by_str(df['Product_omschrijving'].values, input_str)
 
     if max(scores) <= 0:
@@ -71,24 +75,3 @@ def search_objects(input_str):
         scores = search_by_str(df['Product_synoniemen'].values, input_str)
 
     return [Food.objects.get(index=index) for index in convert_scores_to_indices(scores)[:10]]
-
-
-def search_formatted(input_str, max_nr_items=None, columns=None, return_scores=False):
-    df_result, scores = search_by_str_from_df(df, 'Product_omschrijving', input_str)
-
-    if len(df_result) == 0:
-        scores = search_by_str(recombine_words(df), input_str)
-        df_result, scores = convert_scores_to_result(df, scores)
-
-    if len(df_result) == 0:
-        df_result, scores = search_by_str_from_df(df, 'Product_synoniemen', input_str)
-
-    if columns is None:
-        df_output = df_result.iloc[:max_nr_items].copy()
-    else:
-        df_output = df_result.iloc[:max_nr_items][columns].copy()
-
-    if return_scores:
-        df_output['Score'] = scores[:max_nr_items]
-    
-    return df_output
