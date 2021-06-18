fe3dd39560daa54ad6c032ffd1dec2baa039c939
Freek-H <freek.hoogstede@gmail.com>
2021-06-18 13:26:16 +0200

    testing new db

--- nevo_api/views.py
+++ nevo_api/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
 
+import dotenv
+import os
+
+dotenv.load_dotenv()
+
 
 def index(request):
-    return HttpResponse(f"<a href='/api/docs/'>/api/docs/</a>")
+    return HttpResponse(f"{os.environ.get('FREEK_AZURE_NAME')} {os.environ.get('FREEK_AZURE_USER')[:3]} {os.environ.get('FREEK_AZURE_PASSWORD')[:3]} {os.environ.get('FREEK_AZURE_HOST')[:3]} {os.environ.get('FREEK_AZURE_PORT')[:3]} <a href='/api/docs/'>/api/docs/</a>")
