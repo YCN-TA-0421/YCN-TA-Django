cbba83f9ea5544a57caf935b195cfdf9822e97d3
Freek-H <freek.hoogstede@gmail.com>
2021-06-19 16:04:45 +0200

    second test

--- nevo_api/views.py
+++ nevo_api/views.py
@@ -7,4 +7,5 @@ dotenv.load_dotenv()
 
 
 def index(request):
-    return HttpResponse(f"{os.environ.get('FREEK_AZURE_NAME')} {os.environ.get('FREEK_AZURE_USER')[:3]} {os.environ.get('FREEK_AZURE_PASSWORD')[:3]} {os.environ.get('FREEK_AZURE_HOST')[:3]} {os.environ.get('FREEK_AZURE_PORT')[:3]} <a href='/api/docs/'>/api/docs/</a>")
+    # return HttpResponse(f"{os.environ.get('FREEK_AZURE_NAME')} {os.environ.get('FREEK_AZURE_USER')[:3]} {os.environ.get('FREEK_AZURE_PASSWORD')[:3]} {os.environ.get('FREEK_AZURE_HOST')[:3]} {os.environ.get('FREEK_AZURE_PORT')[:3]} <a href='/api/docs/'>/api/docs/</a>")
+    return HttpResponse(f"<a href='/api/docs/'>/api/docs/</a>")
